9d216ee64481a185e6b57925501fdcd7c0b97c4f
Manasa-2309 <manasaskumar230903@gmail.com>
2025-11-25 22:30:08 +0530

    project created

--- src/main/java/com/jsp/book/repository/UserRepository.java
+++ src/main/java/com/jsp/book/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.jsp.book.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jsp.book.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/com/jsp/book/util/EmailHelper.java
+++ src/main/java/com/jsp/book/util/EmailHelper.java
@@ -1,5 +1,8 @@
 package com.jsp.book.util;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmailHelper {
 
 }
