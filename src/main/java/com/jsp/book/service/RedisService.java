5fd8368320d9aa801a2f1078311045141380522c
Manasa-2309 <manasaskumar230903@gmail.com>
2025-12-29 11:02:59 +0530

    seat selection

--- src/main/java/com/jsp/book/service/RedisService.java
+++ src/main/java/com/jsp/book/service/RedisService.java
@@ -1,6 +1,9 @@
 package com.jsp.book.service;
+
 import com.jsp.book.dto.UserDto;
+
 public interface RedisService {
+
 	void saveUserDto(String email, UserDto userDto);
 
 	void saveOtp(String email, int otp);
@@ -8,5 +11,7 @@ public interface RedisService {
 	UserDto getDtoByEmail(String email);
 
 	int getOtpByEmail(String email);
+	
+	
 
 }
